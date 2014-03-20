8c6276aa06ea289b10245f4bab003378fb79a598
Peggy Griffin <peggysuegriffin@gmail.com>
2014-03-20 16:57:14 -0600

    made filter terms no longer case sensitive via a .downcase method on the text

--- lib/items_repository.rb
+++ lib/items_repository.rb
@@ -1,12 +1,12 @@
 class ItemsRepository
   def items(filter)
-    if filter == "cheese"
+    if filter.downcase == "cheese"
       [Item.new(3, "Cheese")]
-    elsif filter == "garlic bread"
+    elsif filter.downcase == "garlic bread"
       [Item.new(4, "Garlic Bread")]
-    elsif filter == "french fries"
+    elsif filter.downcase == "french fries"
       [Item.new(2, "French Fries")]
-    elsif filter == "pizza"
+    elsif filter.downcase == "pizza"
       [Item.new(1, "Pizza")]
     else
       [Item.new(1, "Pizza"), Item.new(2, "French Fries"), Item.new(3, "Garlic Bread"), Item.new(4, "Cheese")]
